0e38a87fae797d9d56b4bea2b2064aad591bda4d
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2022-07-17 17:56:22 -0400

    Remapped slime sling

--- src/main/java/einstein/usefulslime/ModInit.java
+++ src/main/java/einstein/usefulslime/ModInit.java
@@ -20,6 +20,6 @@ public class ModInit {
 
     public static final RegistryObject<Block> SLIPPERY_SLIME_BLOCK = BLOCKS.register("slippery_slime_block", () -> new SlipperySlimeBlock(Block.Properties.of(Material.CLAY, MaterialColor.GRASS).friction(1.5F).noOcclusion().sound(SoundType.SLIME_BLOCK)));
     public static final RegistryObject<Item> SLIPPERY_SLIME_BLOCK_ITEM = ITEMS.register("slippery_slime_block", () -> new BlockItem(SLIPPERY_SLIME_BLOCK.get(), new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_TOOLS)));
-    public static final RegistryObject<Item> SLIMESLING = ITEMS.register("slimesling", () -> new SlimeSlingItem(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_TOOLS)));
+    public static final RegistryObject<Item> SLIME_SLING = ITEMS.register("slime_sling", () -> new SlimeSlingItem(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> SLIME_BOOTS = ITEMS.register("slime_boots", () -> new SlimeBoots(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
 }
--- src/main/java/einstein/usefulslime/UsefulSlime.java
+++ src/main/java/einstein/usefulslime/UsefulSlime.java
@@ -6,9 +6,12 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.MissingMappingsEvent;
 
 @Mod(UsefulSlime.MODID)
 public class UsefulSlime {
@@ -26,5 +29,15 @@ public class UsefulSlime {
 
     public void setupBlockRenderLayer(final FMLClientSetupEvent event) {
         ItemBlockRenderTypes.setRenderLayer(ModInit.SLIPPERY_SLIME_BLOCK.get(), RenderType.translucent());
+    }
+
+    @SubscribeEvent
+    void missingMappings(MissingMappingsEvent event) {
+
+        for (MissingMappingsEvent.Mapping mapping : event.getMappings(ForgeRegistries.ITEMS.getRegistryKey(), MODID)) {
+            if (mapping.getKey().getPath().equals("slimesling")) {
+                mapping.remap(ModInit.SLIME_SLING.get());
+            }
+        }
     }
 }
