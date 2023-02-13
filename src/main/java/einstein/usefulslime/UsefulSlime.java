4e8454535e76f84da2f95e1391b2e08957899b85
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-02-12 19:31:34 -0500

    Updated how render type is applied

--- src/main/java/einstein/usefulslime/UsefulSlime.java
+++ src/main/java/einstein/usefulslime/UsefulSlime.java
@@ -4,6 +4,7 @@ import einstein.usefulslime.util.BounceHandler;
 import einstein.usefulslime.util.ModEventHandler;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,7 +20,6 @@ public class UsefulSlime {
 
     public UsefulSlime() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::setupBlockRenderLayer);
         ModInit.ITEMS.register(modEventBus);
         ModInit.BLOCKS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(new ModEventHandler());
@@ -27,13 +27,9 @@ public class UsefulSlime {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    public void setupBlockRenderLayer(final FMLClientSetupEvent event) {
-        ItemBlockRenderTypes.setRenderLayer(ModInit.SLIPPERY_SLIME_BLOCK.get(), RenderType.translucent());
-    }
-
     @SubscribeEvent
     void missingMappings(MissingMappingsEvent event) {
-        for (MissingMappingsEvent.Mapping mapping : event.getMappings(ForgeRegistries.ITEMS.getRegistryKey(), MODID)) {
+        for (MissingMappingsEvent.Mapping<Item> mapping : event.getMappings(ForgeRegistries.ITEMS.getRegistryKey(), MODID)) {
             if (mapping.getKey().getPath().equals("slimesling")) {
                 mapping.remap(ModInit.SLIME_SLING.get());
             }
