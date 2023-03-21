d454e976fc0a853d34094decfaf5d7d7d3b7e2d4
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-03-21 11:16:41 -0400

    Only the Slime Helmet is required to hang climb now

--- Common/src/main/java/einstein/usefulslime/mixin/ClimbingPlayerMixin.java
+++ Common/src/main/java/einstein/usefulslime/mixin/ClimbingPlayerMixin.java
@@ -1,5 +1,6 @@
 package einstein.usefulslime.mixin;
 
+import einstein.usefulslime.ModInit;
 import einstein.usefulslime.items.SlimeArmor;
 import einstein.usefulslime.util.VerticalCollider;
 import net.minecraft.client.Minecraft;
@@ -7,17 +8,21 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public abstract class ClimbingPlayerMixin extends LivingEntity {
+
+    @Shadow public abstract ItemStack getItemBySlot(EquipmentSlot slot);
 
     private static final EntityDataAccessor<Boolean> DATA_CLIMBING = SynchedEntityData.defineId(ClimbingPlayerMixin.class, EntityDataSerializers.BOOLEAN);
 
@@ -39,10 +44,9 @@ public abstract class ClimbingPlayerMixin extends LivingEntity {
 
     @Override
     public boolean onClimbable() {
-        boolean hasFullSlimeArmor = hasFullSlimeArmor();
-        boolean canHangClimb = hasFullSlimeArmor && ((VerticalCollider) this).verticalCollisionAbove() && Minecraft.getInstance().options.keyJump.isDown();
+        boolean canHangClimb = getItemBySlot(EquipmentSlot.HEAD).is(ModInit.SLIME_HELMET.get()) && ((VerticalCollider) this).verticalCollisionAbove() && Minecraft.getInstance().options.keyJump.isDown();
         setNoGravity(canHangClimb);
-        return super.onClimbable() || (hasFullSlimeArmor && entityData.get(DATA_CLIMBING)) || canHangClimb;
+        return super.onClimbable() || (hasFullSlimeArmor() && entityData.get(DATA_CLIMBING)) || canHangClimb;
     }
 
     private boolean hasFullSlimeArmor() {
