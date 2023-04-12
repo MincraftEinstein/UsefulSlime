995f3cd3d0fa605b17382d5df8e671864f785027
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-04-11 23:16:05 -0400

    Redid LivingEntityMixin Fixes #3

--- Fabric/src/main/java/einstein/usefulslime/mixin/LivingEntityMixin.java
+++ Fabric/src/main/java/einstein/usefulslime/mixin/LivingEntityMixin.java
@@ -2,56 +2,44 @@ package einstein.usefulslime.mixin;
 
 import einstein.usefulslime.UsefulSlime;
 import einstein.usefulslime.util.LivingFallData;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity {
+public abstract class LivingEntityMixin {
 
-    @Shadow protected abstract int calculateFallDamage(float $$0, float $$1);
+    private static final ThreadLocal<LivingFallData> currentFallData = new ThreadLocal<>();
 
-    @Shadow protected abstract SoundEvent getFallDamageSound(int $$0);
-
-    @Shadow protected abstract void playBlockFallSound();
-
-    public LivingEntityMixin(EntityType<?> entityType, Level level) {
-        super(entityType, level);
+    @Inject(method = "causeFallDamage(FFLnet/minecraft/world/damagesource/DamageSource;)Z", at = @At("HEAD"), cancellable = true)
+    private void causeFallDamage(float distance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> callbackInfo) {
+        LivingFallData data = new LivingFallData((LivingEntity) (Object) this, distance, damageMultiplier);
+        UsefulSlime.onFall(data);
+        if (data.isCanceled()) {
+            callbackInfo.setReturnValue(false);
+        }
+        currentFallData.set(data);
     }
 
-    /**
-     * @author MincraftEinstein
-     * @reason Needed to change the value of <b>{@code distance}</b> and <b>{@code damageMultiplier}</b> and at
-     * the time of writing this, the only way I know of to maintain parity between Forge and Fabric is to overwrite
-     * the method
-     */
-    @Overwrite
-    public boolean causeFallDamage(float distance, float damageMultiplier, DamageSource source) {
-        LivingEntity entity = (LivingEntity) (Object) (this);
-        LivingFallData data = new LivingFallData(entity, distance, damageMultiplier);
-        UsefulSlime.onFall(data);
-        distance = data.getDistance();
-        damageMultiplier = data.getDamageMultiplier();
-        if (data.isCanceled()) {
-            return false;
+    @ModifyVariable(method = "causeFallDamage", at = @At(value = "LOAD"), ordinal = 0, argsOnly = true)
+    private float modifyFallDistance(float distance) {
+        LivingFallData data = currentFallData.get();
+        if (data != null) {
+            distance = data.getDistance();
         }
+        return distance;
+    }
 
-        // Copied directly from Minecraft's source code (DO NOT CHANGE!!!)
-        boolean causeFallDamage = super.causeFallDamage(distance, damageMultiplier, source);
-        int damage = calculateFallDamage(distance, damageMultiplier);
-        if (damage > 0) {
-            playSound(getFallDamageSound(damage), 1.0F, 1.0F);
-            playBlockFallSound();
-            hurt(source, damage);
-            return true;
-        } else {
-            return causeFallDamage;
+    @ModifyVariable(method = "causeFallDamage", at = @At(value = "LOAD"), ordinal = 1, argsOnly = true)
+    private float modifyFallDamageMultiplier(float damageMultiplyer) {
+        LivingFallData data = currentFallData.get();
+        if (data != null) {
+            damageMultiplyer = data.getDamageMultiplier();
         }
+        return damageMultiplyer;
     }
 }
