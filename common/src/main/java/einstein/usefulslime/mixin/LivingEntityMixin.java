2e5923e59dafb4dd23733a4a934d64421262c56a
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-04-25 13:24:02 -0400

    Slime helmet now protects against kinetic energy

--- common/src/main/java/einstein/usefulslime/mixin/LivingEntityMixin.java
+++ common/src/main/java/einstein/usefulslime/mixin/LivingEntityMixin.java
@@ -1,21 +1,43 @@
 package einstein.usefulslime.mixin;
 
+import einstein.usefulslime.UsefulSlime;
 import einstein.usefulslime.init.ModBlocks;
 import einstein.usefulslime.init.ModCommonConfigs;
+import einstein.usefulslime.init.ModItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+
+    @Shadow
+    public abstract boolean isAlive();
+
+    @Shadow
+    public abstract ItemStack getItemBySlot(EquipmentSlot equipmentSlot);
+
+    @Shadow
+    public abstract boolean isFallFlying();
+
+    @Unique
+    private final LivingEntity usefulSlime$me = (LivingEntity) (Object) this;
 
     public LivingEntityMixin(EntityType<?> type, Level level) {
         super(type, level);
@@ -44,6 +66,17 @@ public abstract class LivingEntityMixin extends Entity {
 
             if (movement.z < -max) {
                 setDeltaMovement(movement.x, movement.y, -max);
+            }
+        }
+    }
+
+    @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
+    private void hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (!isInvulnerableTo(source) && isFallFlying() && source.is(DamageTypes.FLY_INTO_WALL) && isAlive()) {
+            if (getItemBySlot(EquipmentSlot.HEAD).is(ModItems.SLIME_HELMET.get())) {
+                level().playSound(null, getX(), getY(), getZ(), SoundEvents.SLIME_SQUISH, getSoundSource(), 1, 1);
+                UsefulSlime.damageEquipment(usefulSlime$me, EquipmentSlot.HEAD, Math.round(amount / 2));
+                cir.setReturnValue(false);
             }
         }
     }
