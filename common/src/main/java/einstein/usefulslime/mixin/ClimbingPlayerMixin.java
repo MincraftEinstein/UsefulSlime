b254e1abc61f670a1c486629091888af82da03d9
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-08-03 15:39:11 -0400

    Fixed slime armor climbing packets being sent every tick

--- common/src/main/java/einstein/usefulslime/mixin/ClimbingPlayerMixin.java
+++ common/src/main/java/einstein/usefulslime/mixin/ClimbingPlayerMixin.java
@@ -1,12 +1,10 @@
 package einstein.usefulslime.mixin;
 
 import commonnetwork.api.Dispatcher;
-import einstein.usefulslime.init.ModCommonConfigs;
 import einstein.usefulslime.init.ModItems;
 import einstein.usefulslime.networking.serverbound.ServerBoundHangClimbPacket;
 import einstein.usefulslime.networking.serverbound.ServerBoundWallClimbPacket;
 import einstein.usefulslime.util.ClimbingEntity;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -16,17 +14,16 @@ import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import static einstein.usefulslime.UsefulSlime.damageEquipment;
 
 @Mixin(Player.class)
 public abstract class ClimbingPlayerMixin extends LivingEntity {
 
     @Unique
     private final ClimbingEntity usefulSlime$climbingEntity = (ClimbingEntity) this;
+    @Unique
+    private boolean usefulSlime$canHangClimbOld = false;
+    @Unique
+    private boolean usefulSlime$canWallClimbOld = false;
 
     @Shadow
     public abstract ItemStack getItemBySlot(EquipmentSlot slot);
@@ -46,12 +43,18 @@ public abstract class ClimbingPlayerMixin extends LivingEntity {
                     && getItemBySlot(EquipmentSlot.CHEST).is(ModItems.SLIME_CHESTPLATE.get())
                     && horizontalCollision && !super.onClimbable();
 
-            usefulSlime$climbingEntity.usefulSlime$setHangClimbing(canHangClimb);
-            Dispatcher.sendToServer(new ServerBoundHangClimbPacket(canHangClimb));
-            setNoGravity(canHangClimb);
+            if (canHangClimb != usefulSlime$canHangClimbOld) {
+                usefulSlime$canHangClimbOld = canHangClimb;
+                usefulSlime$climbingEntity.usefulSlime$setHangClimbing(canHangClimb);
+                Dispatcher.sendToServer(new ServerBoundHangClimbPacket(canHangClimb));
+                setNoGravity(canHangClimb);
+            }
 
-            usefulSlime$climbingEntity.usefulSlime$setWallClimbing(canWallClimb);
-            Dispatcher.sendToServer(new ServerBoundWallClimbPacket(canWallClimb));
+            if (canWallClimb != usefulSlime$canWallClimbOld) {
+                usefulSlime$canWallClimbOld = canWallClimb;
+                usefulSlime$climbingEntity.usefulSlime$setWallClimbing(canWallClimb);
+                Dispatcher.sendToServer(new ServerBoundWallClimbPacket(canWallClimb));
+            }
         }
         return super.onClimbable() || usefulSlime$climbingEntity.usefulSlime$canWallClimb() || usefulSlime$climbingEntity.usefulSlime$canHangClimb();
     }
